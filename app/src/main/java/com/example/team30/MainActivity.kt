3c4561d249263fa97e3c545f5314b28be64b42a9
newjunsung <assd1108@naver.com>
2022-11-02 12:55:56 +0900

    connect firebase-auth

--- app/src/main/java/com/example/team30/MainActivity.kt
+++ app/src/main/java/com/example/team30/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.team30
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Firebase.auth.signInWithEmailAndPassword("admin@team30.com", "123456")
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    println("########## 성공")
+                } else {
+                    it.exception?.message
+                    println("########## 실패")
+                }
+            }
     }
 }
