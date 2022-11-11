e607a7592a27f6507ded12613482ba01ba2ada1a
BrightWisdom0502 <brightwisdom0502@gmail.com>
2022-11-11 16:45:25 +0900

    글 내용 파이어베이스 서버 업로드 구현

--- app/src/main/java/com/example/team30/post/AddPost.kt
+++ app/src/main/java/com/example/team30/post/AddPost.kt
@@ -1,5 +1,6 @@
 package com.example.team30.post
 
+import android.app.Activity
 import android.content.ContentUris
 import android.content.Intent
 import android.net.Uri
@@ -7,11 +8,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.team30.databinding.ActivityAddPostBinding
 import com.example.team30.home.SNSActivity
 import com.example.team30.post.model.PostDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -25,6 +29,8 @@ class AddPost : AppCompatActivity() {
 
     lateinit var db: FirebaseFirestore
     lateinit var storage: FirebaseStorage
+    lateinit var auth: FirebaseAuth
+
     lateinit var photoUri: Uri
 
     // photo picker
@@ -47,6 +53,7 @@ class AddPost : AppCompatActivity() {
 
         db = Firebase.firestore
         storage = Firebase.storage
+        auth = Firebase.auth
 
         binding.addpostUploadImage.setOnClickListener {
             pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
@@ -62,16 +69,37 @@ class AddPost : AppCompatActivity() {
         val imageFileName = "IMAGE_" + timestamp + "_.png"
 
         val imageRef = storage.reference.child("images/${imageFileName}")
+
+        // Callback
         imageRef.putFile(photoUri).addOnSuccessListener {
             Log.d("Upload Image", "Image Upload Success")
             intent = Intent(this, SNSActivity::class.java);
             startActivity(intent)
-//            imageRef.downloadUrl.addOnSuccessListener {
-//                var postDTO = PostDTO()
-//
-//                // Insert downloadURL of image
-//                postDTO.imageUrl = it.toString()
-//            }
+            imageRef.downloadUrl.addOnSuccessListener {
+                var postDTO = PostDTO()
+
+                // Insert downloadURL of image
+                postDTO.imageUrl = it.toString()
+                // Insert uid of user
+                postDTO.uid = auth.currentUser?.uid
+                // Insert userId
+                postDTO.userId = auth.currentUser?.email
+                // Insert title
+                postDTO.title = binding.addpostTitle.text.toString()
+                // Insert description
+                postDTO.description = binding.addpostDescription.text.toString()
+                // Insert timestamp
+                postDTO.timestamp = System.currentTimeMillis()
+
+                db.collection("posts").document().set(postDTO)
+
+                Toast.makeText(this, "글이 등록되었습니다", Toast.LENGTH_SHORT).show()
+                Log.d("Upload Post", "Post Upload Success")
+                setResult(Activity.RESULT_OK)
+                finish()
+            }
+        }.addOnFailureListener {
+            Log.d("Upload Post", "Post Upload Fail")
         }
     }
 }
