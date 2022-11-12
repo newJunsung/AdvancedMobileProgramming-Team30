44c63e4b9536bbd3083c6d70ff33ad13aec60ec8
장정윤 <jeinie9179@naver.com>
2022-11-12 23:03:31 +0900

    likes button click event

--- app/src/main/java/com/example/team30/home/feeds/FeedsFragment.kt
+++ app/src/main/java/com/example/team30/home/feeds/FeedsFragment.kt
@@ -12,11 +12,13 @@ import com.bumptech.glide.Glide
 import com.example.team30.R
 import com.example.team30.databinding.FragmentFeedsBinding
 import com.example.team30.post.model.PostDTO
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class FeedsFragment: Fragment() {
     var firestore : FirebaseFirestore? = null
+    var uid : String? = null
 
     companion object {
         const val TAG: String = "로그"
@@ -39,6 +41,7 @@ class FeedsFragment: Fragment() {
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_feeds, container, false)
         firestore = FirebaseFirestore.getInstance()
+        uid = FirebaseAuth.getInstance().currentUser?.uid
 
         view.findViewById<RecyclerView>(R.id.feeds_recyclerview).adapter = FeedsRecyclerViewAdapter()
         view.findViewById<RecyclerView>(R.id.feeds_recyclerview).layoutManager = LinearLayoutManager(activity)
@@ -94,6 +97,38 @@ class FeedsFragment: Fragment() {
             // ProfileImage
             Glide.with(holder.itemView.context).load(PostDTOs[position].imageUrl)
                 .into(viewholder.findViewById(R.id.feed_timeline_profile_image))
+
+            // '좋아요' 버튼 클릭 이벤트
+            viewholder.findViewById<ImageView>(R.id.feed_timeline_favorite_imageview).setOnClickListener {
+                favoriteClickEvent(position)
+            }
+
+            if (PostDTOs!![position].favorites.containsKey(uid)) { // 클릭한 경우 하트 이미지
+                viewholder.findViewById<ImageView>(R.id.feed_timeline_favorite_imageview)
+                    .setImageResource(R.drawable.ic_favorite)
+            } else { // 클릭하지 않은 경우 비어 있는 하트
+                viewholder.findViewById<ImageView>(R.id.feed_timeline_favorite_imageview)
+                    .setImageResource(R.drawable.ic_favorite_border)
+            }
+        }
+
+        // '좋아요' 클릭하면 카운트를 다시 세팅
+        fun favoriteClickEvent(position: Int) {
+            // 선택한 컨텐츠의 uid 를 받아와서 넣어준다.
+            var doc = firestore?.collection("posts")?.document(PostUidList[position])
+            firestore?.runTransaction { transaction ->
+                var postDTO = transaction.get(doc!!).toObject(PostDTO::class.java)
+
+                // '좋아요' 가 이미 클릭되어 있는 경우 / 아닌 경우
+                if(postDTO!!.favorites.containsKey(uid)) { // 클릭되어 있는 경우 (클릭 해제하면 됨)
+                    postDTO?.favoriteCount = postDTO.favoriteCount - 1
+                    postDTO?.favorites?.remove(uid)
+                } else { // 클릭되어 있지 않은 경우
+                    postDTO?.favoriteCount = postDTO.favoriteCount + 1
+                    postDTO.favorites[uid!!] = true // favorites 에 uid 값 추가
+                }
+                transaction.set(doc, postDTO) // transaction 을 서버로 돌려준다.
+            }
         }
 
         override fun getItemCount(): Int {
