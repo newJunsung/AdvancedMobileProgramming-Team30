aee7d4c24612598b2b42900e1fd689ef6e1ea5fd
newjunsung <assd1108@naver.com>
2022-11-13 16:18:20 +0900

    프로필 탭 구현

--- app/src/main/java/com/example/team30/home/SNSActivity.kt
+++ app/src/main/java/com/example/team30/home/SNSActivity.kt
@@ -1,15 +1,10 @@
 package com.example.team30.home
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.team30.R
 import com.example.team30.databinding.ActivitySnsBinding
 import com.example.team30.home.feeds.FeedsFragment
@@ -17,6 +12,7 @@ import com.example.team30.home.friends.FriendsFragment
 import com.example.team30.home.profile.ProfileFragment
 import com.example.team30.post.AddPost
 import com.google.android.material.navigation.NavigationBarView
+import com.google.firebase.auth.FirebaseAuth
 
 class SNSActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
     private lateinit var binding: ActivitySnsBinding
@@ -44,6 +40,10 @@ class SNSActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListene
             }
             R.id.tab_bar_profile -> {
                 profileFragment = ProfileFragment.newInstance()
+                var bundle = Bundle()
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                bundle.putString("destinationUid", uid)
+                profileFragment.arguments = bundle
                 supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, profileFragment).commit()
             }
             R.id.tab_bar_friends -> {
--- app/src/main/java/com/example/team30/home/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/team30/home/profile/ProfileFragment.kt
@@ -2,15 +2,28 @@ package com.example.team30.home.profile
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.example.team30.R
+import com.example.team30.post.model.PostDTO
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class ProfileFragment: Fragment() {
+    var fragmentView: View? = null
+    var firestore: FirebaseFirestore? = null
+    var uid: String? = null
+    var auth: FirebaseAuth? = null
+
     companion object {
-        const val TAG: String = "로그"
+        const val TAG: String = "프로필"
 
         fun newInstance(): ProfileFragment {
             return ProfileFragment()
@@ -22,12 +35,60 @@ class ProfileFragment: Fragment() {
         Log.d(TAG, "프로필 창이다.")
     }
 
-    // fragment와 layout을 연결
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_profile, container, false)
+        uid = arguments?.getString("destinationUid")
+        Log.d(TAG, "${uid}")
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        fragmentView?.user_recyclerview?.adapter = ProfileRecyclerViewAdapter()
+        fragmentView?.user_recyclerview?.layoutManager = GridLayoutManager(activity, 3)
+        return fragmentView
+    }
+
+    inner class ProfileRecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var PostDTOs: ArrayList<PostDTO> = arrayListOf()
+
+        init {
+            firestore?.collection("posts")?.whereEqualTo("uid", uid)?.addSnapshotListener { value, error ->
+                if (value == null) {
+                    return@addSnapshotListener
+                }
+
+                Log.d(TAG, "${value.documents.size}")
+                for (snapshot in value.documents) {
+                    PostDTOs.add(snapshot.toObject(PostDTO::class.java)!!)
+                }
+                fragmentView?.user_post_count?.text = PostDTOs.size.toString()
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var width = resources.displayMetrics.widthPixels / 3
+
+            var imageView = ImageView(parent.context)
+            imageView.layoutParams = LinearLayoutCompat.LayoutParams(width, width)
+            return CustomViewHolder(imageView)
+        }
+
+        inner class CustomViewHolder(var imageView: ImageView): RecyclerView.ViewHolder(imageView) {
+
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var imageView = (holder as CustomViewHolder).imageView
+            Glide.with(holder.itemView.context).load(PostDTOs[position].imageUrl).apply(RequestOptions().centerCrop()).into(imageView)
+        }
+
+        override fun getItemCount(): Int {
+            return PostDTOs.size
+        }
+
     }
 }
