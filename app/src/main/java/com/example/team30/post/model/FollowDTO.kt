29ec6db77529afc2f9dc93fac36a3579feb60879
newjunsung <assd1108@naver.com>
2022-11-13 20:10:23 +0900

    친구 관계(팔로워, 팔로잉) DTO 생성

---
+++ app/src/main/java/com/example/team30/post/model/FollowDTO.kt
@@ -0,0 +1,9 @@
+package com.example.team30.post.model
+
+data class FollowDTO(
+    var followerCount: Int = 0,
+    var followers: MutableMap<String, Boolean> = HashMap(),
+
+    var followingCount: Int = 0,
+    var followings: MutableMap<String, Boolean> = HashMap()
+)
