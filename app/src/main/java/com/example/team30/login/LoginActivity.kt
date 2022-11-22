71e1add408c07881a1a00d1a85e3e764b2fff186
장정윤 <jeinie9179@naver.com>
2022-11-22 18:26:49 +0900

    이메일 형식 검사 및 디자인 보완

--- app/src/main/java/com/example/team30/login/LoginActivity.kt
+++ app/src/main/java/com/example/team30/login/LoginActivity.kt
@@ -1,26 +1,35 @@
 package com.example.team30.login
 
-import android.content.ContentValues.TAG
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.team30.home.SNSActivity
 import com.example.team30.databinding.ActivityLoginBinding
-import com.example.team30.home.feeds.FeedsFragment
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
-    lateinit var db:FirebaseFirestore
+    lateinit var db: FirebaseFirestore
     lateinit var alertDialog: AlertDialog
+
+    // 이메일 검사 정규식
+    private val emailValidation =
+        "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,10 +42,32 @@ class LoginActivity : AppCompatActivity() {
         var email = binding.emailEdit.text
         var password = binding.passwordEdit.text
 
+        // 입력하는 글자색 검정으로
+        binding.emailEdit.setTextColor(Color.BLACK)
+        binding.passwordEdit.setTextColor(Color.BLACK)
+
+        // emailEdit 에 TextWatcher 연결
+        emailEdit.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                // text가 변경된 후 호출
+                // p0 : 변경 후 문자열
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // text가 변경되기 전 호출
+                // p0 : 변경 전 문자열
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // text가 바뀔 때마다 호출
+                checkEmail()
+            }
+        })
+
         alertDialog = AlertDialog.Builder(this)
             .setTitle("로그인 실패")
             .setMessage("이메일 또는 비밀번호를 확인해주세요.")
-            .setPositiveButton("확인", object: DialogInterface.OnClickListener {
+            .setPositiveButton("확인", object : DialogInterface.OnClickListener {
                 override fun onClick(dialog: DialogInterface?, which: Int) {
                     Log.d("로그인", "로그인 실패")
                 }
@@ -63,39 +94,6 @@ class LoginActivity : AppCompatActivity() {
         binding.loginbtn.setOnClickListener {
             doLogin(email.toString(), password.toString())
         }
-        /*binding.loginbtn.setOnClickListener {
-            // 사용자가 입력한 이메일이 db에 있는지 먼저 확인
-            Log.d("email : ", email.toString())
-            val docRef = db.collection("users").document(email.toString())
-            Log.d("docRef : ", docRef.toString())
-            docRef.get()
-                .addOnSuccessListener { document ->
-                    Log.d("document : ", document.toString())
-                    if (document != null) {
-                        Log.d("password : ", document.data?.get("password").toString())
-                        // 비밀번호까지 맞으면 로그인 성공
-                        if (document.data?.get("password").toString() == password.toString()) {
-                            Log.d(TAG, "DocumentSnapshot data: ${document.data}")
-                            intent = Intent(baseContext, SNSActivity::class.java)
-                            startActivity(intent) // 로그인 성공하면 피드 페이지로 넘어감
-                        } else { // 비밀번호가 틀리면 다시 입력받음
-                            email = binding.emailEdit.text
-                            password = binding.passwordEdit.text
-                            binding.emailEdit.setText("")
-                            binding.passwordEdit.setText("")
-                            alertDialog.show()
-                        }
-
-                    } else { // 이메일이 없는 경우
-//                        Log.d(TAG, "No such document")
-                        alertDialog.show()
-                    }
-                }
-                .addOnFailureListener { exception ->
-                    Log.d(TAG, "get failed with ", exception)
-
-                }
-        }*/
     }
 
     override fun onStart() {
@@ -103,6 +101,10 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun doLogin(userEmail: String, password: String) {
+        // 이메일 형식 다시 한 번 체크
+        if (!checkEmail()) { // 이메일 형식이 아닐 경우
+            Toast.makeText(this, "이메일 형식을 확인해주세요!", Toast.LENGTH_SHORT).show()
+        }
         Firebase.auth.signInWithEmailAndPassword(userEmail.toString(), password.toString())
             .addOnCompleteListener(this) {
                 if (it.isSuccessful) {
@@ -123,5 +125,20 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, SNSActivity::class.java))
         }
         finish()
+    }
+
+    // 이메일 형식 검사
+    fun checkEmail(): Boolean {
+        var email = binding.emailEdit.text.toString()
+        val pattern = Pattern.matches(emailValidation, email) // 서로 패턴이 맞는지
+
+        if (pattern) { // 이메일 형태가 맞으면
+            binding.emailValidation.text = ""
+            return true
+        } else { // 이메일 형태가 아니면 텍스트뷰를 동적으로 생성하고 아래에 문구를 띄운다.
+            binding.emailValidation.text = "* 이메일 형식을 확인해주세요!"
+            binding.emailValidation.setTextColor(-65536)
+            return false
+        }
     }
 }
--- app/src/main/java/com/example/team30/login/SignUpActivity.kt
+++ app/src/main/java/com/example/team30/login/SignUpActivity.kt
@@ -2,7 +2,10 @@ package com.example.team30.login
 
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +16,9 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_signup.*
+import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
@@ -20,6 +26,9 @@ class SignUpActivity : AppCompatActivity() {
 
     companion object {
         const val BASIC_IMAGE_URL = "https://firebasestorage.googleapis.com/v0/b/team30-project.appspot.com/o/userProfileImages%2FBasic.png?alt=media&token=c8dc87bc-405d-442a-ba5a-993077099854"
+        // 이메일 검사 정규식
+        private val emailValidation =
+            "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +39,29 @@ class SignUpActivity : AppCompatActivity() {
         val name = binding.nameEdit.text
         val email = binding.emailSignUpEdit.text
         val password = binding.pwSignUpEdit.text
+
+        // 입력하는 글자색 검정으로
+        binding.nameEdit.setTextColor(Color.BLACK)
+        binding.emailSignUpEdit.setTextColor(Color.BLACK)
+        binding.pwSignUpEdit.setTextColor(Color.BLACK)
+
+        // emailSignUpEdit 에 TextWatcher 연결
+        emailSignUpEdit.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                // text가 변경된 후 호출
+                // p0 : 변경 후 문자열
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // text가 변경되기 전 호출
+                // p0 : 변경 전 문자열
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // text가 바뀔 때마다 호출
+                checkEmail()
+            }
+        })
 
         // cloud firestore 초기화
         db = Firebase.firestore
@@ -84,5 +116,20 @@ class SignUpActivity : AppCompatActivity() {
                     Toast.makeText(this, "비밀번호는 6자 이상이어야 합니다", Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    // 이메일 형식 검사
+    fun checkEmail(): Boolean {
+        var email = binding.emailSignUpEdit.text.toString()
+        val pattern = Pattern.matches(emailValidation, email) // 서로 패턴이 맞는지
+
+        if (pattern) { // 이메일 형태가 맞으면
+            binding.joinEmailValidation.text = ""
+            return true
+        } else { // 이메일 형태가 아니면 텍스트뷰를 동적으로 생성하고 아래에 문구를 띄운다.
+            binding.joinEmailValidation.text = "* 이메일 형식을 확인해주세요!"
+            binding.joinEmailValidation.setTextColor(-65536)
+            return false
+        }
     }
 }
